8eb5e2d8a6beec606a9e18187a9b750d26290f9f
Sabu Siyad <hello@ssiyad.com>
2026-01-01 11:33:17 +0530

    feat: return path

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 use worker::*;
 
 #[event(fetch)]
-async fn fetch(
-    _req: Request,
-    _env: Env,
-    _ctx: Context,
-) -> Result<Response> {
-    Response::ok("Hello World!")
+async fn fetch(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+    Response::ok(match req.path().as_str() {
+        "/" => "Hello, World!".to_string(),
+        _ => format!("Hello, {}", req.path().trim_start_matches('/')),
+    })
 }
+
